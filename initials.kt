61695b73319af37138f2ca3477a4f72f586be037
Ville Uusitalo <75932298+Nyyhaus@users.noreply.github.com>
2021-03-14 06:28:34 +0200

    Create initials.kt

---
+++ initials.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) 
+{
+	var firstName: String = "Kate"
+	var lastName: String = "Hudson"
+	
+	var firstNameChar = firstName[0]
+	var lastNameChar = lastName[0]
+	
+	println("Your initials: $firstNameChar.$lastNameChar")
+}
